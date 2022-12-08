b19dbd9d99d7442bcb5c497a4393d30ed1f0a164
muhammetemrevatan <emre.vatan98@hotmail.com>
2022-12-08 11:24:31 +0300

    CurrentUser edited!

--- src/main/java/com/memrevatan/toxifyou/core/annotation/CurrentUser.java
+++ src/main/java/com/memrevatan/toxifyou/core/annotation/CurrentUser.java
@@ -2,7 +2,6 @@ package com.memrevatan.toxifyou.core.annotation;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
